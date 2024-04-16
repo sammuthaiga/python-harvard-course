e76dd54bb636c8a8f66574b2256abe6c756ae492
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-16 23:49:39 +0300

    Input function, Variables, Value assignment, Concatination

--- hello.py
+++ hello.py
@@ -1,3 +1,27 @@
+# # use python (name of the file) hello.py to run code on the terminal
+
+
+# # print function
+# print("Hello, world!")
+
+# # input function, variables, value assignment,
+name = input("What is your name? ")
+print("Hello,", name, "!")
+birth_year = float(input("Enter your year of birth: "))
+age = 2024 - birth_year
+# concatination
+
+# Pass 3 arguments to the print function
+print("your are", age, "years old")
+
+#  Use the + operator to concatenate
+#  Convert the age into a string
+print("your are " + str(age) + " years old")
+# print(name)
 # print function
-# use python (name of the file) hello.py to run code on the terminal
-print("Hello, world!")
+# print("Hello, world!")
+
+# input function, variables, value assignment,
+# name = raw_input("What is your name? ")
+# print("Hello, ", name, "!")
+# print(name)
