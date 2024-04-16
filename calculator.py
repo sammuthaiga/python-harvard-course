3b01a4836a38a2f7ed11fc3464bb09e0daafbdeb
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-17 02:42:42 +0300

    diffrent ways to round to a specific number of decimal places

--- calculator.py
+++ calculator.py
@@ -10,34 +10,45 @@
 # print(z)
 
 # refactor 
-x = int(input("Enter value of x: "))
-y = int(input("Enter value of y: "))
-print(x + y)
+# x = int(input("Enter value of x: "))
+# y = int(input("Enter value of y: "))
+# print(x + y)
 
-# subtractions
-a = x - y
+# # subtractions
+# a = x - y
 
-# multiplications
-b = x * y
+# # multiplications
+# b = x * y
 
-# divisions
-c = x / y
+# # divisions
+# c = x / y
 
-# modulus
-d = x % y
-print(a, b, c, d)
+# # modulus
+# d = x % y
+# print(a, b, c, d)
 
-#  Working with floating point numbers
-k = float(input("Enter value of x: "))
-l = float(input("Enter value of y: "))
-print(k + l)
+# #  Working with floating point numbers
+# k = float(input("Enter value of x: "))
+# l = float(input("Enter value of y: "))
+# print(k + l)
 
-# round to nearest integer
-z = round(k + l)
-print(z)
+# # round to nearest integer
+# z = round(k + l)
+# print(z)
 
-# add a , to separate 000
-e = 999
-f = 1
-g = e + f
-print(f"{g:,}")
+# # add a , to separate 000
+# e = 999
+# f = 1
+# g = e + f
+# print(f"{g:,}")  
+
+# round to nearest a specific number of decimal places
+
+h = 2.93456
+i = h * 2345
+
+#  1 way to round to a specific number of decimal places 3
+j = round(h + i, 3)
+
+print(j)
+print(f"{j:.4f}")
