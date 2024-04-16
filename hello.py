241de39cdfaae55b8b3abac5f220d7d21a78fb7a
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-17 00:57:14 +0300

    embed expressions inside strings by prefixing the string with an f

--- hello.py
+++ hello.py
@@ -6,22 +6,27 @@
 
 # # input function, variables, value assignment,
 name = input("What is your name? ")
-print("Hello,", name, "!")
-birth_year = float(input("Enter your year of birth: "))
-age = 2024 - birth_year
+print(f"Hello {name}!")
+# birth_year = float(input("Enter your year of birth: "))
+# age = 2024 - birth_year
 # concatination
 
 # Pass 3 arguments to the print function
-print("your are", age, "years old")
+# print("your are", age, "years old")
 
 #  Use the + operator to concatenate
 #  Convert the age into a string
-print("your are " + str(age) + " years old")
-# print(name)
-# print function
-# print("Hello, world!")
+# print("your are " + str(age) + " years old")
+
+#  Python 2 
+#  Difference between raw_input(python2) and input(python3) functions
 
 # input function, variables, value assignment,
 # name = raw_input("What is your name? ")
 # print("Hello, ", name, "!")
 # print(name)
+
+# embed expressions inside strings by prefixing the string with an f 
+support = input("How can we assist you? ")
+stack = input("Enter your tech stack: ")
+print (f"Hi, your issue on {support} has been reviewed. Your knowledge in {stack} is really required here {name}!!")
