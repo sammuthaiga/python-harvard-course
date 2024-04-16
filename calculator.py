de6984616fa4988463882f58037064616492c94d
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-17 02:01:39 +0300

    convert user input to integer to perform arithmetic operations

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Working with Intergers 
+# Aritmetic symbols include +, -, *, /, % and **
+
+#  Addition 
+# x = input("Enter value of x: ")
+# y = input("Enter value of y: ")
+# #  N/B User input values are always of type string
+# #  Hence we need to convert them to integers
+# z= int(x) + int(y)
+# print(z)
+
+# refactor 
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+z = x + y
+print(z)
+
