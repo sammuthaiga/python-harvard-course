a7818b0ad423715ed279796624a0d29d536c5812
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-18 22:01:28 +0300

    use the for loop with the built in range function

--- loops/cat_mimic.py
+++ loops/cat_mimic.py
@@ -1,11 +1,15 @@
 #  while loop
-i = 4
-while i != 0:
-    print("Meow")
-    i = i - 1
+# i = 4
+# while i != 0:
+#     print("Meow")
+#     i = i - 1
 
-# Another way
-j = 0
-while j < 4:
-    print("Jumping")
-    j += 1
+# # Another way
+# j = 0
+# while j < 4:
+#     print("Jumping")
+#     j += 1
+
+# For... in  loop
+for i in range(3):
+    print("Running")
