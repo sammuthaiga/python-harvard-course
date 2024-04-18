ea9f3ba816fca1e018454b9f9e23bd5c3977aa8b
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-18 18:48:25 +0300

    implement match to replace the if statement

---
+++ conditionals/tasks.py
@@ -0,0 +1,16 @@
+# use match instead of the if statement
+
+task = input("To know your task, enter your name: ").capitalize().strip()
+
+# # match task:
+#     case "Daniel" | "John" | "Sam" | "David" | "Sally":
+#         print("Your are a UI Developer")
+#     case "Aggie" | "Mary" | "Mike" | "Smith":
+#         print("Your are a FrontEnd Developer")
+#     case "John" | "John" | "Luke" | "Steff":
+#         print("You are a BackendEnd Developer")
+#     case "Jeru" | "Josse" | "Dan" | "John" | "Nancy":
+#         print("Your are a FullStack Developer")
+#     case _:
+#         print("Did you apply?")
+
