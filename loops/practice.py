16d2474d50aafc97cf112a0a074e7ca8a98da6bf
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-19 00:46:45 +0300

    More on loops and intro to list iteration

---
+++ loops/practice.py
@@ -0,0 +1,26 @@
+#  More examples on continous loops
+#  Hiring system if score is above 50.in progress though.[-_-] *[._.]*
+def main():
+  results = get_results()
+  congraturate(results)
+
+
+def get_results():
+  while True:
+    n = int(input("Enter your score: "))
+    if n >= 50:
+        return n
+    else:
+        print("Please try again after 10 minutes")
+  
+def congraturate(results):
+  name = input("Whats your name? ").capitalize().split()
+  candidates = ["sam", "ben", "cara", "mike"]
+  for i in range(len(candidates)):
+    print(f"congrats!, {name[i]}")
+  
+
+
+
+main()
+
