1a04ab8d622d61d6f4cb5e47f5adf1b4ea05e62f
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-20 00:39:18 +0300

    refactor: Shorten code

--- dict/mario.py
+++ dict/mario.py
@@ -1,17 +1,23 @@
 # create a function to immitate the column-block in super mario
 
 def main():
-    print_block(3)
-    print_row(4)
+    # print_block(3)
+    # print_row(4)
     print_square(3)
 
 #  Loop inside a loop.
+# def print_square(size):
+# print a row of size (3)
+#     for i in range(size):
+#  print a brick of size 3 in each row 3 times.
+#         for j in range(size):
+#             print("#", end="")
+#  Print a new blank line which moves the cursor down the next line
+#         print ()
+#  Shorter way
 def print_square(size):
     for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print ()
-
+        print("#"  * size)
 
 
 
@@ -28,9 +34,9 @@ def print_square(size):
 #     for _ in range(height):
 #         print("##")
  
-def print_block(height):
-    print("##\n" * height, end="")
+# def print_block(height):
+#     print("##\n" * height, end="")
 
-def print_row(width):
-    print("?" * 4)
+# def print_row(width):
+#     print("?" * 4)
 main()
