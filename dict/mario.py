1c52bb58b839ae911ea094547af2792a0f0a5967
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-20 00:30:50 +0300

    include a loop inside another loop.

--- dict/mario.py
+++ dict/mario.py
@@ -3,6 +3,26 @@
 def main():
     print_block(3)
     print_row(4)
+    print_square(3)
+
+#  Loop inside a loop.
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print ()
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def print_block(height):
 #     for _ in range(height):
