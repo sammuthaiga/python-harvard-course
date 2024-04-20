5aabc07dda26aa116f5b7dd242c9467f676aeb6c
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-20 22:43:27 +0300

    introduce a while True loop to continue running if user input is invalid

--- exceptions/nunbers.py
+++ exceptions/nunbers.py
@@ -1,7 +1,17 @@
-# Error handling using exceptions and else 
-try:
-    x = int(input("What is the value of x? "))
-except ValueError:
-    print("X is not an interger")
-else:
-    print(f"x ix {x}")
+# # Error handling using exceptions and else 
+# try:
+#     x = int(input("What is the value of x? "))
+# except ValueError:
+#     print("X is not an interger")
+# else:
+#     print(f"x ix {x}")
+
+# include a loop to keep asking for value of x if user doesn't input an interger
+while True:
+    try:
+        x = int(input("What is the values of X? "))
+    except ValueError:
+        print("Error! X is not an integer")
+    else:
+        break
+print(f"X is {x}")
