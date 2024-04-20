36c0c5db78668a5cdc2dedf4dfe3b7cfb3c83a9f
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-21 00:45:18 +0300

    Make the function more dynamic

--- exceptions/prac_func.py
+++ exceptions/prac_func.py
@@ -18,17 +18,31 @@
 
 # shorter version
 
-def main():
-    x = get_int()
-    print(f"x is {x}")
+# def main():
+#     x = get_int()
+#     print(f"x is {x}")
 
-def get_int():
+# def get_int():
+#     while True:
+#         try:
+#             return int(input(" What is x?  "))
+#         except ValueError:
+#             print("x must be an integer")
+
+# main()
+
+#  Make it more dynamic
+def main():
+    x = get_int("What is x? ")
+    print(f"X is {x}")
+
+def get_int(prompt):
     while True:
         try:
-            return int(input(" What is x?  "))
+            return int(input(prompt))
         except ValueError:
             print("x must be an integer")
 
 main()
-
+    
 
