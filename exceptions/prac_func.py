bd12d0e8201b6628d56074fb2f9cca90e6e98fa3
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-04-21 00:25:51 +0300

    shorten further

--- exceptions/prac_func.py
+++ exceptions/prac_func.py
@@ -1,18 +1,33 @@
+# def main():
+#     x = get_int()
+#     print(f"X is {x}")
+
+
+# def get_int():
+#     while True:
+#         try:
+#             x = int(input("Enter the value of x? "))
+#         except ValueError:
+#             print("X must be a integer")
+#         else:
+#             break
+#     return x
+
+
+# main()
+
+# shorter version
+
 def main():
     x = get_int()
-    print(f"X is {x}")
-
+    print(f"x is {x}")
 
 def get_int():
     while True:
         try:
-            x = int(input("Enter the value of x? "))
+            return int(input(" What is x?  "))
         except ValueError:
-            print("X must be a integer")
-        else:
-            break
-    return x
-
+            print("x must be an integer")
 
 main()
 
